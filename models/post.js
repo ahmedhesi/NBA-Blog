bd5e431c48f4fd172c7a363495ad614eafff29b7
ahmedhesi <shiikh63@gmail.com>
2023-07-13 15:06:49 -0500

    Deploy

--- models/post.js
+++ models/post.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+  content: {type: String, required: true}, 
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+}, {
+  timestamps: true,
+})
 
 const postSchema = new Schema({
-  content: {type: String, required: true},
+  content: {type: String, required: true}, 
+  comments: [commentSchema],
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
